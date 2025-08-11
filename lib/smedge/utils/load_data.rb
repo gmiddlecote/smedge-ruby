99115a2adeb3bae6406853e5f7ffe6bbbfdbf798
George Middlecote <gmiddlecote@gmail.com>
2025-08-11 19:39:10 +0530

    added orders and sorbet

--- lib/smedge/utils/load_data.rb
+++ lib/smedge/utils/load_data.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
+# typed: strict
 
 # load_data.rb
 
 require "yaml"
+require "sorbet-runtime"
 
 module Smedge
   # module Utils
   module Utils
     # module LoadData
     module LoadData
+      extend T::Sig
+
       # module LoadData
+      sig { params(yaml_file_path: String).returns(T::Hash[String, Smedge::Client]) }
       def self.load_clients(yaml_file_path)
         data = YAML.load_file(yaml_file_path)
         return {} unless data["clients"]
@@ -23,16 +28,29 @@ module Smedge
         clients
       end
 
+      sig do
+        params(
+          yaml_file_path: String,
+          clients: T::Hash[String, Smedge::Client]
+        ).returns(T::Hash[String, Smedge::Transaction])
+      end
       def self.load_transactions(yaml_file_path, clients)
-        data = YAML.load_file(yaml_file_path)
-        return unless data["transactions"]
+        data = T.let(
+          YAML.safe_load_file(yaml_file_path, aliases: true) || {},
+          T::Hash[T.untyped, T.untyped]
+        )
 
-        data["transactions"].each do |txn|
+        transactions = T.let({}, T::Hash[String, Smedge::Transaction])
+        return transactions unless data["transactions"].is_a?(Array)
+
+        data["transactions"].each_with_index do |txn, idx|
+          next unless txn.is_a?(Hash)
           client = clients[txn["client"]]
           next unless client
 
-          if txn["type"] == "income"
-            income = Income.new(
+          case txn["type"]
+          when "income"
+            income = Smedge::Income.new(
               date: txn["date"],
               amount: txn["amount"],
               mode: txn["mode"],
@@ -41,8 +59,9 @@ module Smedge
               order_id: nil
             )
             client.add_credit(income)
+            transactions["income_#{idx}"] = income
 
-          elsif txn["type"] == "expense"
+          when "expense"
             expense = Smedge::Expense.new(
               date: txn["date"],
               amount: txn["amount"],
@@ -51,10 +70,13 @@ module Smedge
               client: client
             )
             client.add_debit(expense)
-
+            transactions["expense_#{idx}"] = expense
           end
         end
+
+        transactions
       end
+
     end
   end
 end
